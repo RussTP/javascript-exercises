8994b62c7706b3ca5985a3f8abb87a2511ba24d8
RussTP <rpawlett@live.com>
2025-01-08 17:54:48 -0500

    used split, reverse, and join to create an array, reverse and stringify the word argument

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+return word.split("").reverse().join("");
 };
+
+reverseString("Hello There");
+
+// Split turns the string into an Array which can be reveresed, .join will "re-string" in reverse
 
 // Do not edit below this line
 module.exports = reverseString;
