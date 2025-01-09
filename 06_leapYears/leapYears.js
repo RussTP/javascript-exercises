8142fe44977873d6056bf9974207f75fdac5eace
RussTP <rpawlett@live.com>
2025-01-09 18:13:27 -0500

    years can be entered to confirm if it is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year)
+ { const century = year % 100 === 0;
+if (year % 4 === 0 && (!century || year % 400 === 0)) {
+return year = true; 
+} else {
+return year = false; 
+}
 };
-
+console.log(leapYears(1984))
 // Do not edit below this line
 module.exports = leapYears;
